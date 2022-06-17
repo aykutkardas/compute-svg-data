0fab6e0f41fbe08196900aeee7dc741efc8291bc
Aykut Kardaş <aykutkrds@gmail.com>
2022-06-17 13:25:55 +0300

    Add types and fix export

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import getComputedSvgData from "./getComputedSvgData";
 import getCurrentIcon from "./getCurrentIcon";
 import getSvgPaths from "./getSvgPaths";
 import getComputedSvgStyle from "./getComputedSvgStyle";
+import { Options, IconSet, Icon } from "./types";
 
-export default {
+export {
   getComputedSvgData,
   getCurrentIcon,
   getSvgPaths,
   getComputedSvgStyle,
+  Options,
+  IconSet,
+  Icon,
 };
